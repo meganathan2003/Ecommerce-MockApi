cd6160076a11bf96fa06018d97756d48f3788165
Meganathan Subramanian <mesubramanian@fssa.freshworks.com>
2023-12-08 18:50:38 +0530

    added new js

---
+++ E-commerceApp/connectionUtil/connection.js
@@ -0,0 +1,10 @@
+
+
+// Below the code for connect the connection form local host
+
+const mysql = require('mysql');
+
+// Create connection to the data base
+ const connection = mysql.createConnection({
+    
+ })
